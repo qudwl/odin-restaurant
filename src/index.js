bac27820242279248def99382be3023c2fdde3a2
Byeongji Lim <me@qudwl.com>
2022-07-24 09:51:23 +0900

    Most basic tab switching functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 import home from "./home";
+import contact from "./contact";
+import menu from "./menu";
 
 const page = document.getElementById("content");
 
+let tabGroup = document.createElement('div');
+let homeTab = document.createElement('button');
+let menuTab = document.createElement('button');
+let contactTab = document.createElement('button');
+
+// Add event listeners to remove and add tabs.
+homeTab.addEventListener('click', () => {
+    page.removeChild(page.lastChild);
+    page.appendChild(home());
+});
+menuTab.addEventListener('click', () => {
+    page.removeChild(page.lastChild);
+    page.appendChild(menu());
+});
+contactTab.addEventListener('click', () => {
+    page.removeChild(page.lastChild);
+    page.appendChild(contact());
+});
+
+// Set button text
+homeTab.textContent = "Home";
+menuTab.textContent = "Menu";
+contactTab.textContent = "Contact";
+
+tabGroup.appendChild(homeTab);
+tabGroup.appendChild(menuTab);
+tabGroup.appendChild(contactTab);
+
+page.appendChild(tabGroup);
 page.appendChild(home());
