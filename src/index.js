9bfc9b941f5eb36c0ae5565ab9a15a4d19e0d44f
Byeongji Lim <me@qudwl.com>
2022-07-24 11:00:08 +0900

    Add slight bit of design

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import home from "./home";
 import contact from "./contact";
 import menu from "./menu";
+import "./style.css";
 
 const page = document.getElementById("content");
 
@@ -13,15 +14,39 @@ let contactTab = document.createElement('button');
 homeTab.addEventListener('click', () => {
     page.removeChild(page.lastChild);
     page.appendChild(home());
+    setCurTab("home");
 });
 menuTab.addEventListener('click', () => {
     page.removeChild(page.lastChild);
     page.appendChild(menu());
+    setCurTab("menu");
 });
 contactTab.addEventListener('click', () => {
     page.removeChild(page.lastChild);
     page.appendChild(contact());
+    setCurTab("contact");
 });
+
+const setCurTab = (tab) => {
+    homeTab.classList = "";
+    menuTab.classList = "";
+    contactTab.classList = "";
+
+    switch (tab) {
+        case "home":
+            homeTab.classList = "active";
+            break;
+        case "menu":
+            menuTab.classList = "active";
+            break;
+        case "contact":
+            contactTab.classList = "active";
+            break;
+        default:
+            console.error("Should not be able to come here.", 1);
+            break;
+    }
+}
 
 // Set button text
 homeTab.textContent = "Home";
@@ -32,5 +57,9 @@ tabGroup.appendChild(homeTab);
 tabGroup.appendChild(menuTab);
 tabGroup.appendChild(contactTab);
 
+tabGroup.classList = "tabGroup";
+
 page.appendChild(tabGroup);
 page.appendChild(home());
+
+setCurTab("home");
